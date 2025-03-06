bbf887c5931b68706d893d81e52c205728d151b4
marylyne <otienomarylyne76@gmail.com>
2025-03-06 08:58:29 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-// Code your solution in this file!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function distanceFromHqInBlocks(pickupLocation) {
+  const hqLocation = 42;
+  return Math.abs(pickupLocation - hqLocation);
+}
+
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(destination - start) * 264;
+}
+
+
+
+
+
+
+
+
+function distanceFromHqInFeet(pickupLocation) {
+  const feetPerBlock = 264;
+  return Math.abs(pickupLocation - 42) * feetPerBlock;
+}
+
+
+
+
+
+
+function calculatesFarePrice(start, destination) {
+  const distanceInFeet = Math.abs(destination - start) * 264;
+
+  if (distanceInFeet <= 400) {
+      return 0;
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+      return (distanceInFeet - 400) * 0.02;
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+      return 25;
+  } else {
+      return "cannot travel that far";
+  }
+}
